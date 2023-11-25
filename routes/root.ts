bf9befa153d0120b7d058e956ea67f7252d391a8
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-11-25 18:30:23 +0100

    restructured routes

---
+++ routes/root.ts
@@ -0,0 +1,76 @@
+import { intiBlueskyAgent } from './../lib/bluesky'
+import { authenticateJWT } from './../lib/utils'
+import { db } from './../lib/db'
+import { FastifyInstance } from 'fastify'
+
+export const routesRoot = async (app: FastifyInstance, options: Object) => {
+    app.get('/', { onRequest: [authenticateJWT] }, async (req, res) => {
+        const user = await db.user.findFirst({ where: { id: req.user.id } })
+
+        return res.view("index", {
+            userName: req.user.mastodonHandle,
+            instance: req.user.instance,
+            blueskyHandle: user?.blueskyHandle,
+            hasBlueskyToken: user?.blueskyToken ? true : false,
+            pollingInterval: parseInt(process.env.POLL_INTERVAL ?? '60')
+        })
+    })
+
+    app.post<{
+        Body: {
+            blueskyHandle: string,
+            blueskyToken: string
+        }
+    }>('/', { onRequest: [authenticateJWT] }, async (req, res) => {
+        const user = await db.user.findFirst({ where: { id: req.user.id } })
+
+        let response_data = {
+            err: undefined,
+            userName: req.user.mastodonHandle,
+            instance: req.user.instance,
+            pollingInterval: parseInt(process.env.POLL_INTERVAL ?? '60')
+        };
+
+        const passwordRegex = new RegExp(/^(?:[a-zA-Z0-9]{4}-){3}[a-zA-Z0-9]{4}$/);
+        const passwordValid = passwordRegex.test(req.body.blueskyToken)
+
+        if (!passwordValid) return res.status(400).view("index", {
+            ...response_data,
+            err: 'Invalid Bluesky Token'
+        })
+
+        const handleRegex = new RegExp(/^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/);
+        const handleValid = handleRegex.test(req.body.blueskyHandle)
+
+        if (!handleValid) return res.status(400).view("index", {
+            ...response_data,
+            err: 'Invalid Bluesky Handle'
+        })
+
+        const agent = await intiBlueskyAgent('https://bsky.social', req.body.blueskyHandle, req.body.blueskyToken).catch((err) => {
+            console.log(err)
+            return res.status(400).view("index", {
+                ...response_data,
+                err: 'Could not authenticate to Bluesky'
+            })
+        })
+
+        await db.user.update({
+            where: {
+                id: req.user.id
+            },
+            data: {
+                blueskyHandle: req.body.blueskyHandle,
+                blueskyToken: req.body.blueskyToken
+            }
+        })
+
+        response_data = {
+            ...response_data,
+            blueskyHandle: user?.blueskyHandle,
+            hasBlueskyToken: user?.blueskyToken ? true : false
+        }
+
+        return res.redirect('/')
+    })
+}
---
+++ routes/user.ts
@@ -0,0 +1,162 @@
+import { FastifyInstance } from "fastify";
+import { Mastodon } from 'megalodon'
+import { authenticateJWT, domainToUrl, genCallBackUrl } from './../lib/utils'
+import { db, getInstanceByDomain, getUserByMastodonUid } from './../lib/db'
+
+export const routesUser = async (app: FastifyInstance, options: Object) => {
+    app.get('/login', async (req, res) => {
+        return res.view("login", {});
+    })
+
+    app.get('/logout', async (req, res) => {
+        return res.clearCookie('token').redirect('/login')
+    })
+
+    app.post('/account/delete', { onRequest: [authenticateJWT] }, async (req, res) => {
+        await db.user.delete({
+            where: {
+                id: req.user.id
+            }
+        })
+        return res.clearCookie('token').redirect('/login')
+    })
+
+    app.get('/account/downloadData', { onRequest: [authenticateJWT] }, async (req, res) => {
+        const user = await db.user.findFirst(
+            {
+                where: { id: req.user.id },
+                select: {
+                    mastodonInstance: {
+                        select: {
+                            url: true,
+                        }
+                    },
+                    createdAt: true,
+                    updatedAt: true,
+                    mastodonUid: true,
+                    mastodonToken: true,
+                    name: true,
+                    lastTootTime: true,
+                    blueskyHandle: true,
+                    blueskyToken: true,
+                }
+            })
+        res.header('Content-Disposition', `attachment; filename=skymoth-userdata-${user?.name}.json`)
+        res.send(user).type('application/json').code(200).redirect('/')
+    })
+
+    app.get<{
+        Querystring: {
+            instance: string
+        }
+    }>('/auth', async (req, res) => {
+        let instanceDomain: string = req.query.instance || "mastodon.social"
+        instanceDomain = instanceDomain.toLowerCase().replace(/https?:\/\//, "")
+        const url = domainToUrl(instanceDomain)
+        let client = new Mastodon(url)
+
+        let knownInstance = await getInstanceByDomain(instanceDomain)
+
+        if (knownInstance) {
+            console.log("--- INSTANCE ALREADY REGISTERED ---")
+        } else {
+            console.log("--- INSTANCE NOT REGISTERED ---")
+            const appData = await client.createApp(process.env.APP_ID ?? "skymoth-test", {
+                scopes: ["read"],
+                redirect_uris: genCallBackUrl(instanceDomain)
+            })
+
+            knownInstance = await db.mastodonInstance.create({
+                data: {
+                    url: instanceDomain,
+                    urlEncoded: btoa(instanceDomain),
+                    applicationId: appData.client_id,
+                    applicationSecret: appData.client_secret
+                }
+            })
+        }
+
+        const authUrl = await client.generateAuthUrl(knownInstance.applicationId, knownInstance.applicationSecret, {
+            scope: ["read"],
+            redirect_uri: genCallBackUrl(instanceDomain)
+        })
+        return res.redirect(authUrl)
+    })
+
+    app.get<{
+        Params: {
+            id: string
+        },
+        Querystring: {
+            code: string
+        }
+    }>('/auth/callback/:id', async (req, res) => {
+        const { id } = req.params;
+
+        const instance = await getInstanceByDomain(atob(id)).catch((err) => {
+            console.log(err)
+            res.status(400).view("login", {
+                err: 'A problem occured while authenticating to the instance via token'
+            })
+        })
+
+        if (instance == null) {
+            return res.status(400).view("login", {
+                err: 'A problem occured while authenticating to the instance via token'
+            })
+        }
+
+        let client = new Mastodon(domainToUrl(instance.url))
+
+        const token = await client.fetchAccessToken(instance.applicationId, instance.applicationSecret, req.query.code, genCallBackUrl(instance.url));
+
+        const userClient = new Mastodon(domainToUrl(instance.url), token.access_token)
+        const verifiedCredentials = await userClient.verifyAccountCredentials();
+
+        let user = await getUserByMastodonUid(verifiedCredentials.data.id, instance.id)
+
+        if (!user) {
+            user = await db.user.create({
+                data: {
+                    mastodonInstance: {
+                        connect: {
+                            id: instance.id
+                        }
+                    },
+                    mastodonUid: verifiedCredentials.data.id,
+                    mastodonToken: token.access_token,
+                    name: verifiedCredentials.data.username,
+                    lastTootTime: new Date()
+                }
+            })
+        } else {
+            console.log('updating user')
+
+            user = await db.user.update({
+                where: {
+                    id: user.id
+                },
+                data: {
+                    mastodonToken: token.access_token,
+                    name: verifiedCredentials.data.username,
+                }
+            })
+        }
+
+        const jwt = app.jwt.sign({
+            id: user.id,
+            mastodonHandle: verifiedCredentials.data.username,
+            token: token,
+            instance: instance.url,
+        })
+
+        return res
+            .setCookie('token', jwt, {
+                domain: Bun.env.APP_DOMAIN,
+                path: '/',
+                secure: false,
+                httpOnly: true
+            })
+            .redirect('/')
+    })
+}
