f8c9fc9d663079e0a8f861bcf0a3b95bb4f77c86
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2024-01-26 02:12:25 +0100

    handle reposter errors

--- lib/tasks/mastodonToBluesky.ts
+++ lib/tasks/mastodonToBluesky.ts
@@ -13,59 +13,69 @@ export default async function taskMastodonToBluesky() {
     })
 
     users.forEach(async (user) => {
-        if (!user.blueskyHandle || !user.blueskyToken) {
-            console.log({
-                scheduledJob: 'reposter',
-                status: 'no bluesky creds',
-                user: user.name,
-                instance: user.mastodonInstance.url
-            })
-            return
-        }
-        const userClient = new Mastodon(domainToUrl(user.mastodonInstance.url), user.mastodonToken)
-        let posts = await getNewToots(userClient, user.mastodonUid, user.lastTootTime)
+        try {
+            if (!user.blueskyHandle || !user.blueskyToken) {
+                console.log({
+                    scheduledJob: 'reposter',
+                    status: 'no bluesky creds',
+                    user: user.name,
+                    instance: user.mastodonInstance.url
+                })
+                return
+            }
+            const userClient = new Mastodon(domainToUrl(user.mastodonInstance.url), user.mastodonToken)
+            let posts = await getNewToots(userClient, user.mastodonUid, user.lastTootTime)
 
-        if(posts.length === 0) {
-            console.log({
-                scheduledJob: 'reposter',
-                status: 'no new posts',
-                user: user.name,
-                instance: user.mastodonInstance.url
-            })
-            return
-        }
-
-        const blueskyClient = await intiBlueskyAgent('https://bsky.social', user.blueskyHandle, user.blueskyToken)
-
-        posts = posts.reverse()
-
-        posts.forEach(async (post) => {
-            console.log({
-                scheduledJob: 'reposter',
-                status: `posting ${post.id}`,
-                user: user.name,
-                instance: user.mastodonInstance.url
-            })
-            const postsBsky = await generateBlueskyPostsFromMastodon(post, blueskyClient)
-
-            if (postsBsky.length === 0) return
-
-            let repRef: ReplyRef = {
-                root: undefined!,
-                parent: undefined!
+            if(posts.length === 0) {
+                console.log({
+                    scheduledJob: 'reposter',
+                    status: 'no new posts',
+                    user: user.name,
+                    instance: user.mastodonInstance.url
+                })
+                return
             }
 
-            for (const postBsky of postsBsky) {
-                if (repRef.parent !== undefined) postBsky.reply = repRef;
+            const blueskyClient = await intiBlueskyAgent('https://bsky.social', user.blueskyHandle, user.blueskyToken)
 
-                let result = await blueskyClient.post(postBsky);
-                console.log(result)
+            posts = posts.reverse()
 
-                if (repRef.root === undefined) repRef.root = result;
-                repRef.parent = result;
-            }
+            posts.forEach(async (post) => {
+                console.log({
+                    scheduledJob: 'reposter',
+                    status: `posting ${post.id}`,
+                    user: user.name,
+                    instance: user.mastodonInstance.url
+                })
+                const postsBsky = await generateBlueskyPostsFromMastodon(post, blueskyClient)
 
-            await updateLastPostTime(user.id, new Date(post.created_at))
-        })
+                if (postsBsky.length === 0) return
+
+                let repRef: ReplyRef = {
+                    root: undefined!,
+                    parent: undefined!
+                }
+
+                for (const postBsky of postsBsky) {
+                    if (repRef.parent !== undefined) postBsky.reply = repRef;
+
+                    let result = await blueskyClient.post(postBsky);
+                    console.log(result)
+
+                    if (repRef.root === undefined) repRef.root = result;
+                    repRef.parent = result;
+                }
+
+                await updateLastPostTime(user.id, new Date(post.created_at))
+            })
+        } catch (e) {
+            console.log({
+                scheduledJob: 'reposter',
+                status: 'error',
+                user: user.name,
+                instance: user.mastodonInstance.url,
+                error: e
+            })
+        }
     })
 }
