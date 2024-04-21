0c215987ff00cfb3b2527dda40b82041ce24a169
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2024-04-21 20:59:47 +0200

    add sentry to scheduler

--- lib/tasks/scheduler.ts
+++ lib/tasks/scheduler.ts
@@ -1,5 +1,19 @@
 import { AsyncTask, SimpleIntervalJob, ToadScheduler } from "toad-scheduler"
 import taskMastodonToBluesky from "./mastodonToBluesky"
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+
+if (process.env.SENTRY_DSN) {
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        integrations: [
+            nodeProfilingIntegration(),
+        ],
+        tracesSampleRate: 1.0,
+        profilesSampleRate: 1.0,
+    });
+}
 
 const job = new SimpleIntervalJob({
     seconds: parseInt(process.env.POLL_INTERVAL ?? '60'),
