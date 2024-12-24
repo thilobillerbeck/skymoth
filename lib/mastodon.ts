049de226226b7fee73eb54bef09720237a18d9eb
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2024-12-24 01:49:07 +0100

    fix: minor improvements

--- lib/mastodon.ts
+++ lib/mastodon.ts
@@ -16,15 +16,16 @@ export async function getUserIdFromMastodonHandle(handle: string, client: Megalo
     return a_data[0].id;
 }
 
-function verifyThread(uid: string, status: Status, searchSpace: Status[], initialCall: boolean = false): boolean {
+function verifyThread(uid: string, status: Status, searchSpace: Status[], relayVisibility: StatusVisibility[], initialCall: boolean = false): boolean {
     if (!status) return false;
     if (status.in_reply_to_account_id === uid && (
-        status.visibility === 'unlisted' || status.visibility === 'public'
+        status.visibility === 'unlisted' || relayVisibility.includes(status.visibility)
     )) {
         return verifyThread(
             uid,
             searchSpace.find((s) => s.id === status.in_reply_to_id),
             searchSpace,
+            relayVisibility,
             false
         )
     } else if (!status.in_reply_to_account_id && !initialCall && status.visibility === 'public') {
@@ -64,7 +65,7 @@ export async function getNewToots(client: Mastodon, uid: string, lastTootTime: D
         }
 
         // due to the way some mastodon clients handle threads, we need to check if the status may be a thread
-        const isThread = verifyThread(uid, status, statuses_data, true);
+        const isThread = verifyThread(uid, status, statuses_data, relayVisibility, true);
 
         return newPost && (isInVisibilityScope || isThread) && isNotMention;
     });
