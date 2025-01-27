55e63527fdb0ff7e6f071269aa9d373c1cf516fa
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-01-27 02:37:18 +0100

    refactor: make drizzle config use POSTGRES_URL as well

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,12 +4,7 @@ export default defineConfig({
   out: './drizzle',
   schema: './drizzle/schema.ts',
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT!),
-    user: process.env.DB_USERNAME!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
-    ssl: false
+    url: process.env.POSTGRES_URL!,
   },
   // Print all statements
   verbose: true,
